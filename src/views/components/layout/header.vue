6e3c641cb31c4542ea385b94c21677ed8878c8d5
晏群 <cnyanqun@qq.com>
2020-11-24 21:00:06 +0800

    .

---
+++ src/views/components/layout/header.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-header height="60px" class="header">
+    <el-row :gutter="10">
+      <el-col :sm="8" :xs="12" class="header_le">
+        <div class="logo mid">logo</div>
+      </el-col>
+      <el-col :sm="16" :xs="12" class="header_ri">
+        <div class="mid">menu</div>
+      </el-col>
+    </el-row>
+  </el-header>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.header {
+  background: yellowgreen;
+  div {
+    height: 100%;
+  }
+  .header_le,
+  .header_ri {
+    display: table;
+    .mid {
+      display: table-cell;
+      vertical-align: middle;
+    }
+  }
+}
+</style>
